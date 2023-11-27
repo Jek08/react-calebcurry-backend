f114628381901fe9cda056a86094645d46f3d833
Jek08 <jk.pratama86@gmail.com>
2023-11-27 12:31:14 +0700

    customer get api

--- customers/views.py
+++ customers/views.py
@@ -3,7 +3,11 @@ from django.http import JsonResponse
 from customers.serializer import CustomerSerializer
 
 def customers(request):
-    # invoke serializer and return to client
     data = Customer.objects.all()
     serializer = CustomerSerializer(data, many=True)
     return JsonResponse({"customers": serializer.data})
+
+def customer(request, id):
+    data = Customer.objects.get(pk=id)
+    serializer = CustomerSerializer(data)
+    return JsonResponse({"customer": serializer.data})
